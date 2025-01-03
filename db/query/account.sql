5dbbb9145aa9d9082cc6bfc771bae5abb592c161
oladotun <oladotunadekomaya@gmail.com>
2025-01-03 14:50:15 +0100

    commit

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,10 +1,10 @@
--- name: GetAccount :one
-SELECT * FROM accounts
-WHERE id = $1 LIMIT 1;
+-- -- name: GetAccount :one
+-- SELECT * FROM accounts
+-- WHERE id = $1 LIMIT 1;
 
--- name: ListAccounts :many
-SELECT * FROM accounts
-ORDER BY name;
+-- -- name: ListAccounts :many
+-- SELECT * FROM accounts
+-- ORDER BY name;
 
 -- name: CreateAccounts :one
 INSERT INTO accounts (
@@ -16,12 +16,12 @@ INSERT INTO accounts (
 )
 RETURNING *;
 
--- name: UpdateAccount :exec
-UPDATE accounts
-  set name = $2,
-  bio = $3
-WHERE id = $1;
+-- -- name: UpdateAccount :exec
+-- UPDATE accounts
+--   set name = $2,
+--   bio = $3
+-- WHERE id = $1;
 
--- name: DeleteAccount :exec
-DELETE FROM accounts
-WHERE id = $1;
+-- -- name: DeleteAccount :exec
+-- DELETE FROM accounts
+-- WHERE id = $1;
