a279ad512dc209d724e1532ca9263fac311d6610
oladotun <oladotunadekomaya@gmail.com>
2025-01-06 17:08:05 +0100

    commit

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -1,9 +1,32 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5"
+)
+
+const DATABASE_URL = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 
 var testQueries *Queries
 
 func TestMain(m *testing.M) {
+	// urlExample := "postgres://username:password@localhost:5432/database_name"
+	conn, err := pgx.Connect(context.Background(), DATABASE_URL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
 
+	var name string
+	var weight int64
+	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
 }
