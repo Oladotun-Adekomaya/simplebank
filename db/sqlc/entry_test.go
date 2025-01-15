a914b0c083f0a3061e8d98ad6571226a61fc064e
oladotun <oladotunadekomaya@gmail.com>
2025-01-15 14:16:56 +0100

    commit

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -1,1 +1,42 @@
 package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Oladotun-Adekomaya/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T) Entry {
+	account := createRandomAccount(t)
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, account.ID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry := createRandomEntry(t)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+}
