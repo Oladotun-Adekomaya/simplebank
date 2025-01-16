6a76a59ab704681d665824358ec132e6e4f5ad91
oladotun <oladotunadekomaya@gmail.com>
2025-01-16 08:42:54 +0100

    commit

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -31,7 +32,18 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	err = fn(q)
 	if err != nil {
 		if rbErr := tx.Rollback(ctx); rbErr != nil {
-
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
 		}
+		return err
 	}
+
+	return tx.Commit(ctx)
+}
+
+// TransferTxParams contains the input parameters of the transfer transaction
+
+// TransferTx performs a money transfer from one account to the other
+// It creates a new transfer record, add account entries, and update account's balance within a single database transaction
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+
 }
