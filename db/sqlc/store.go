74e5a5c046ae5611ca7e9bd3b8196c610539fade
oladotun <oladotunadekomaya@gmail.com>
2025-01-16 08:52:29 +0100

    commit

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -41,6 +41,20 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 }
 
 // TransferTxParams contains the input parameters of the transfer transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// TransferTxResult is the result of the transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
 
 // TransferTx performs a money transfer from one account to the other
 // It creates a new transfer record, add account entries, and update account's balance within a single database transaction
