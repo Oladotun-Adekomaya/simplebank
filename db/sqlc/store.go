08ed42a5c00c455094019d9a4ccf88e83f5a5b9c
oladotun <oladotunadekomaya@gmail.com>
2025-01-16 08:10:02 +0100

    commit

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,9 +1,24 @@
 package db
 
-import "github.com/jackc/pgx/v5"
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+)
 
 // Store provides all functions to execute db queries and transactions
 type Store struct {
 	*Queries
 	db *pgx.Conn
+}
+
+// NewStore creates a new Store
+func NewStore(db *pgx.Conn) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+
 }
