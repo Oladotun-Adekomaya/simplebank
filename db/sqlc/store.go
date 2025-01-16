c8d1e5aba36581d83f6051003d5b0ecf1b035bdc
oladotun <oladotunadekomaya@gmail.com>
2025-01-16 07:47:36 +0100

    commit

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,5 +1,9 @@
 package db
 
-// Store provides all functions to execute db queries and
+import "github.com/jackc/pgx/v5"
+
+// Store provides all functions to execute db queries and transactions
 type Store struct {
+	*Queries
+	db *pgx.Conn
 }
